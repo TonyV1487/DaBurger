6ee282474d53fa913342528a34a9b767b7721c7b
TonyV <tvester1487@gmail.com>
2019-12-21 14:42:02 -0600

    controller & models

---
+++ controllers/burgersController.js
@@ -0,0 +1,8 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (daBurger.js) to use its database functions.
+var burger = require("../models/daBurger");
+
+// Routes and required logic
